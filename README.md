8813479d435b19d42b222034d09449a449d2f8bd
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-03 18:47:33 +0100

    Add prisma to project

---
+++ README.md
@@ -0,0 +1,5 @@
+> npx express-generator
+> npm i nodemon --save-dev
+> npm i prisma --save-dev
+> npx prisma  => get help
+> npx prisma init
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', async function(req, ress, next){});
+
+router.get('/:id', async function(req, ress, next){});
+
+router.post('/', async function(req, res, next) {
+  
 });
+
+
+router.put('/:id', async function(req, res, next) {
+  
+});
+
+router.delete('/:id', async function(req, ress, next){});
 
 module.exports = router;
