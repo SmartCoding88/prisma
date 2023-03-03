518b79187dd1a00ecbdb21da8fb8f6abf9d038c8
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-03 21:06:14 +0100

    Read all users

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,47 @@
 var express = require('express');
 var router = express.Router();
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 /* GET users listing. */
 
-router.get('/', async function(req, ress, next){
-    
+router.get('/', async function (req, res, next) {
+
+    const users = await prisma.user.findMany({
+
+        select: {
+            email: true,
+            name: true,
+        },
+
+    });
+
+    return res.send(users);
+
 });
 
-router.get('/:id', async function(req, ress, next){});
+router.get('/:id', async function (req, ress, next) { });
 
-router.post('/', async function(req, res, next) {
+router.post('/', async function (req, res, next) {
     const user = await prisma.user.create({
-        data:{
-            email: 'elsa@prisma.io',
-            name: 'Elsa Prisma',
-        }
+        data: req.body
     });
     return res.send(user);
 });
 
 
-router.put('/:id', async function(req, res, next) {
-  
+router.put('/:id', async function (req, res, next) {
+    const updateUser = await prisma.user.update({
+        where: {
+            id: parseInt(req.params.id),
+        },
+        data: {
+            name: 'Viola the Magnificent',
+        },
+    });
+    res.send(updateUser);
 });
 
-router.delete('/:id', async function(req, ress, next){});
+router.delete('/:id', async function (req, ress, next) { });
 
 module.exports = router;
